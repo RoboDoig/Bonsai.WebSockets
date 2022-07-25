d8b4d91088299f7610d23cf851fc852bd3c8b523
RoboDoig <erskine94@gmail.com>
2022-07-25 19:22:21 +0100

    Add project files.

---
+++ Bonsai.WebSockets/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Reactive.Linq;
+
+namespace Bonsai.WebSockets
+{
+    [Combinator]
+    public class Client
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public IObservable<long> Process(IObservable<long> source)
+        {
+            return Observable.Using(() =>
+            {
+                return new TcpClient(Host, Port);
+            },
+            client =>
+            {
+                NetworkStream stream = client.GetStream();
+                return source.Do(val =>
+                {
+                    Byte[] bytes = System.Text.Encoding.UTF8.GetBytes("test-gribble");
+                    stream.Write(bytes, 0, bytes.Length);
+                });
+            });
+        }
+    }
+}
---
+++ Bonsai.WebSockets/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Bonsai.WebSockets
+{
+    public class Server : Source<int>
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+
+        public override IObservable<int> Generate()
+        {
+            return Observable.Create<int>((observer, cancellationToken) =>
+            {
+                // Setup server
+                var server = new TcpListener(IPAddress.Parse(IpAddress), Port);
+                server.Start();
+
+                cancellationToken.Register(() => { server.Stop(); });
+
+                // TODO - server listener logic
+                return Task.Factory.StartNew(() =>
+                {
+                    var client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        while (!stream.DataAvailable) ;
+
+                        Byte[] bytes = new Byte[client.Available];
+                        stream.Read(bytes, 0, bytes.Length);
+
+                        observer.OnNext(bytes.Length);
+                    }
+                });
+            });
+        }
+    }
+}
