4dc4371e557bc161b83aa824c756e3591647d91b
RoboDoig <erskine94@gmail.com>
2022-07-25 22:14:03 +0100

    client prototype

---
+++ Bonsai.WebSockets/WebSocketClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Reactive.Linq;
+
+namespace Bonsai.WebSockets
+{
+    [Combinator]
+    public class WebSocketClient
+    {
+        public string Address { get; set; }
+
+        public IObservable<long> Process(IObservable<long> source)
+        {
+            return Observable.Using(() =>
+            {
+                ClientWebSocket socket = new ClientWebSocket();
+                socket.ConnectAsync(new Uri(Address), new System.Threading.CancellationToken()).GetAwaiter().GetResult();
+                return socket;
+            },
+            socket =>
+            {
+                return source.Do(val =>
+                {
+                    Console.WriteLine(socket.State);
+                });
+            });
+        }
+    }
+}
