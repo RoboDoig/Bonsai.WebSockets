839ee7b7297fd56d513370159d42b63f725e309a
RoboDoig <erskine94@gmail.com>
2022-07-26 06:39:36 +0100

    prototype rest client

---
+++ Bonsai.WebSockets/RestClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using System.Text.Json;
+
+namespace Bonsai.WebSockets
+{
+    public class RestClient : Combinator<string>
+    {
+        public string Address { get; set; }
+        public string Route { get; set; }
+        public RestClient.RequestType RequestMethod { get; set; }
+
+        public override IObservable<string> Process<TSource>(IObservable<TSource> source)
+        {
+            return Observable.Using(() =>
+            {
+                return new RestSharp.RestClient(Address);
+            },
+            client =>
+            {
+                return source.Select(val =>
+                {
+                    //var request = new RestRequest("/public/v2/users/2");
+                    //Observable.FromAsync(() => client.GetAsync(request));
+                    ////TODO async
+                    
+                    var request = new RestRequest(Route);
+
+                    if (RequestMethod == RequestType.GET)
+                    {
+                        var response = client.Get(request);
+                        return response.Content;
+                    } else
+                    {
+                        request.AddJsonBody(new TestRequest { Name = "Bob", Salary = "123", Age = "30" });
+                        var response = client.Post(request);
+                        return response.Content;
+                    }
+                });
+            });
+        }
+
+        public class TestRequest
+        {
+            public string Name;
+            public string Salary;
+            public string Age;
+        }
+
+        public enum RequestType
+        {
+            GET,
+            POST
+        }
+    }
+}
