75fbe52fec42631fde325311e71f62d26a105408
RoboDoig <erskine94@gmail.com>
2022-09-20 15:22:17 +0100

    Input message stream

--- Bonsai.WebSockets/EmpaticaClient.cs
+++ Bonsai.WebSockets/EmpaticaClient.cs
@@ -20,16 +20,26 @@ namespace Bonsai.WebSockets
 
         public override IObservable<string> Generate()
         {
+            return Generate(null);
+        }
+
+        public IObservable<string> Generate(IObservable<string> messageSequence)
+        {
             return Observable.Create<string>((observer, cancellationToken) =>
             {
                 var client = new TcpClient(Host, Port);
                 var stream = client.GetStream();
 
-                byte[] writeBuffer = Encoding.ASCII.GetBytes("device_connect CB1408" + Environment.NewLine);
-                stream.Write(writeBuffer, 0, writeBuffer.Length);
+                if (messageSequence != null)
+                {
+                    var messager = messageSequence.Do(message =>
+                    {
+                        byte[] writeBuffer = Encoding.ASCII.GetBytes(message + Environment.NewLine);
+                        stream.Write(writeBuffer, 0, writeBuffer.Length);
+                    }).Subscribe();
 
-                writeBuffer = Encoding.ASCII.GetBytes("device_subscribe bvp ON" + Environment.NewLine);
-                stream.Write(writeBuffer, 0, writeBuffer.Length);
+                    cancellationToken.Register(() => { messager.Dispose(); });
+                }
 
                 return Task.Factory.StartNew(() =>
                 {
@@ -54,8 +64,61 @@ namespace Bonsai.WebSockets
                             }
                         }
                     }
+                }).ContinueWith(task =>
+                {
+                    client.Dispose();
+                    task.Dispose();
                 });
             });
         }
+
+        //public override IObservable<string> Generate()
+        //{
+        //    return Generate(null);
+        //}
+
+        //public override IObservable<string> Generate(IObservable<string> messageSequence)
+        //{
+        //    return Observable.Create<string>((observer, cancellationToken) =>
+        //    {
+        //        var client = new TcpClient(Host, Port);
+        //        var stream = client.GetStream();
+
+        //        byte[] writeBuffer = Encoding.ASCII.GetBytes("device_connect CB1408" + Environment.NewLine);
+        //        stream.Write(writeBuffer, 0, writeBuffer.Length);
+
+        //        writeBuffer = Encoding.ASCII.GetBytes("device_subscribe bvp ON" + Environment.NewLine);
+        //        stream.Write(writeBuffer, 0, writeBuffer.Length);
+
+        //        return Task.Factory.StartNew(() =>
+        //        {
+        //            while (!cancellationToken.IsCancellationRequested)
+        //            {
+        //                if (stream.DataAvailable)
+        //                {
+        //                    byte[] readBuffer = new byte[client.Available];
+        //                    StringBuilder sb = new StringBuilder();
+        //                    int bytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+        //                    if (bytesRead > 0)
+        //                    {
+        //                        var readString = Encoding.ASCII.GetString(readBuffer, 0, bytesRead);
+        //                        using (StringReader reader = new StringReader(readString))
+        //                        {
+        //                            string line;
+        //                            while ((line = reader.ReadLine()) != null)
+        //                            {
+        //                                observer.OnNext(line);
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //        }).ContinueWith(task =>
+        //        {
+        //            client.Dispose();
+        //            task.Dispose();
+        //        });
+        //    });
+        //}
     }
 }
