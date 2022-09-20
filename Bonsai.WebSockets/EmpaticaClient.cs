bd7ba713d9988020a64e0e913a749d4fec8f17b2
RoboDoig <erskine94@gmail.com>
2022-09-20 13:53:16 +0100

    basic stream reading for empatica

---
+++ Bonsai.WebSockets/EmpaticaClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Reactive.Disposables;
+using System.Threading;
+
+namespace Bonsai.WebSockets
+{
+    public class EmpaticaClient : Source<string>
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        //private readonly ManualResetEvent ConnectDone = new ManualResetEvent(false);
+        //private readonly ManualResetEvent SendDone = new ManualResetEvent(false);
+        //private readonly ManualResetEvent ReceiveDone = new ManualResetEvent(false);
+
+        public override IObservable<string> Generate()
+        {
+            return Observable.Create<string>((observer, cancellationToken) =>
+            {
+                var client = new TcpClient(Host, Port);
+                var stream = client.GetStream();
+
+                byte[] writeBuffer = Encoding.ASCII.GetBytes("device_connect CB1408" + Environment.NewLine);
+                stream.Write(writeBuffer, 0, writeBuffer.Length);
+
+                writeBuffer = Encoding.ASCII.GetBytes("device_subscribe bvp ON" + Environment.NewLine);
+                stream.Write(writeBuffer, 0, writeBuffer.Length);
+
+                return Task.Factory.StartNew(() =>
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        if (stream.DataAvailable)
+                        {
+                            byte[] readBuffer = new byte[4096];
+                            int bytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+                            if (bytesRead > 0)
+                            {
+                                observer.OnNext(Encoding.ASCII.GetString(readBuffer));
+                            }
+                        }
+                    }
+                });
+            });
+        }
+
+
+    }
+}
