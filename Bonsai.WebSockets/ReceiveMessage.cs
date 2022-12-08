0f75fb1c341162f17f53b65f7a214a15e21ce357
RoboDoig <erskine94@gmail.com>
2022-12-08 13:51:09 +0000

    ReceiveMessage implementation

--- Bonsai.WebSockets/ReceiveMessage.cs
+++ Bonsai.WebSockets/ReceiveMessage.cs
@@ -10,16 +10,21 @@ namespace Bonsai.WebSockets
 {
     public class ReceiveMessage : Combinator<ClientWebSocket, string>
     {
-        //Func<Task<string>> tester = () => { return new Task<string>(() => { return "20"; }); };
-        //const int chunkSize = 1024 * 4;
-        //ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[chunkSize]);
+        const int chunkSize = 1024 * 4;
+        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[chunkSize]);
 
-        //public override IObservable<string> Process(IObservable<ClientWebSocket> source)
-        //{
-        //    return Observable.FromAsync<string>(async () =>
-        //    {
-        //        source.SelectMany(x => x.ReceiveAsync(buffer, ))
-        //    });
-        //}
+        public override IObservable<string> Process(IObservable<ClientWebSocket> source)
+        {
+            return Observable.Create<string>(async (observer, cancellationToken) =>
+            {
+                var result = await source.Select(x =>
+                {
+                    x.ReceiveAsync(buffer, cancellationToken);
+                    return buffer.ToString();
+                });
+
+                observer.OnNext(result);
+            }).Repeat();
+        }
     }
 }
