d61cc9264229a10cfb7245ed4017e5a82e827ab0
RoboDoig <erskine94@gmail.com>
2022-12-08 13:19:36 +0000

    proto separate connection and receive

---
+++ Bonsai.WebSockets/ReceiveMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.WebSockets
+{
+    public class ReceiveMessage : Combinator<ClientWebSocket, string>
+    {
+        //Func<Task<string>> tester = () => { return new Task<string>(() => { return "20"; }); };
+        //const int chunkSize = 1024 * 4;
+        //ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[chunkSize]);
+
+        //public override IObservable<string> Process(IObservable<ClientWebSocket> source)
+        //{
+        //    return Observable.FromAsync<string>(async () =>
+        //    {
+        //        source.SelectMany(x => x.ReceiveAsync(buffer, ))
+        //    });
+        //}
+    }
+}
--- Bonsai.WebSockets/SocketClient.cs
+++ Bonsai.WebSockets/SocketClient.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
 
 namespace Bonsai.WebSockets
 {
-    internal class SocketClient
+    public class SocketClient : Source<ClientWebSocket>
     {
+        public string UriString { get; set; }
+
+        public override IObservable<ClientWebSocket> Generate()
+        {
+            return Observable.Create<ClientWebSocket>(async (observer, cancellationToken) =>
+            {
+                var client = new ClientWebSocket();
+                await client.ConnectAsync(new Uri(UriString), cancellationToken);
+
+                observer.OnNext(client);
+
+                var token = new CancellationToken();
+                return Disposable.Create(async () => await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "close", token));
+            });
+        }
     }
 }
